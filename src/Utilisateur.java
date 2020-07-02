312848c9a5d012bd2782a1ad6ff669636f594cee
younes mezine <mezineyounes@gmail.com>
2020-07-02 11:29:13 +0200

    ajout des props de la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,9 +1,14 @@
 
 /**
- * 
+ * classe Utilisateur 
  * @author INTIFORMATION
  *
  */
 public class Utilisateur {
 
+	/*________________ props _________________________*/
+	private int id;
+	private String nom;
+	private String prenom; 
+	
 }//end class
