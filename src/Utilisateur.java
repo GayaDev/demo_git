8174d565e51de4141c3a7bff051cd1afc33830e9
younes mezine <mezineyounes@gmail.com>
2020-07-02 11:34:40 +0200

    ajout des ctors à la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,14 +1,27 @@
 
 /**
- * classe Utilisateur 
+ * classe Utilisateur
+ * 
  * @author INTIFORMATION
  *
  */
 public class Utilisateur {
 
-	/*________________ props _________________________*/
+	/* ________________ props _________________________ */
 	private int id;
 	private String nom;
-	private String prenom; 
-	
-}//end class
+	private String prenom;
+
+	/* _______________ ctors _________________________ */
+
+	public Utilisateur() {
+	}
+
+	public Utilisateur(int id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+}// end class
